ec621519b2a34af579eaf04193292412f49990a0
User <v.kushnir22@gmail.com>
2025-11-17 20:22:06 +0200

    rmove image

--- app/components/Swiper.vue
+++ app/components/Swiper.vue
@@ -28,7 +28,6 @@ const slides = [
   { image: "/img/2.jpg" },
   { image: "/img/3.jpg" },
   { image: "/img/4.jpg" },
-  { image: "/img/5.jpg" },
   { image: "/img/6.jpg" },
   { image: "/img/7.jpg" },
 ];
